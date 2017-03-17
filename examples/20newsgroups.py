44f241db223b0d8b26608929c87f5f58ecfcddbc
david.fernig@shopify.com <david.fernig@shopify.com>
2017-03-17 15:13:51 -0400

    adding plotting to examples (wip)

--- examples/20newsgroups.py
+++ examples/20newsgroups.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals, division
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from sklearn.datasets import fetch_20newsgroups_vectorized
+from active_learning.active_learning import ActiveLearner
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
@@ -8,20 +14,17 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.svm import SVC
 import numpy as np
 import tempfile
-import sys
-import os
+
 
 from scipy.sparse import vstack
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from active_learning.active_learning import ActiveLearner
-
-from sklearn.datasets import fetch_20newsgroups_vectorized
 
 if __name__ == '__main__':
     all_random, all_uncertainty = [], []
     AL = ActiveLearner(strategy='max_margin')
-    clf = SGDClassifier(loss='log')
+    clf = LogisticRegression()
     data = fetch_20newsgroups_vectorized()
 
     for _ in range(10):
@@ -29,11 +32,13 @@ if __name__ == '__main__':
             data.data, data.target, test_size=0.5)
         
         X_labeled, X_unlabeled, y_labeled, y_oracle = train_test_split(
-            X_train, y_train, test_size=0.5)
+            X_train, y_train, test_size=0.9)
         
-        random, uncertainty = [], []
-        print 'base:', X_labeled.shape[0]
-        for num_queries in (200, 400, 600, 800, 1000):
+        num_samples, random, uncertainty = [], [], []
+
+        for num_queries in (20, 40, 60, 80, 100):
+            num_samples.append(num_queries)
+
             random_queries = np.random.choice(X_unlabeled.shape[0], num_queries, replace=False)
             X_augmented = vstack((X_labeled, X_unlabeled[random_queries, :]))
             y_augmented = np.concatenate((y_labeled, y_oracle[random_queries]))
@@ -50,5 +55,8 @@ if __name__ == '__main__':
         all_uncertainty.append(uncertainty)
         all_random.append(random)
 
-    print np.mean(np.array(all_random), axis=0)
-    print np.mean(np.array(all_uncertainty), axis=0)
+
+    sns.set_style("darkgrid")
+    plt.plot(num_samples, all_random[0], 'r', num_samples, all_uncertainty[0], 'b')
+    plt.legend(['Random Sampling', 'Least Confident'])
+    plt.show() 
