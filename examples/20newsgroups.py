83b8fbf3c9085d1dc43452c5500a3edbba358116
david.fernig@shopify.com <david.fernig@shopify.com>
2017-03-18 18:35:03 -0400

    adding plots to 20newsgroups examples

--- examples/20newsgroups.py
+++ examples/20newsgroups.py
@@ -8,55 +8,66 @@ from sklearn.datasets import fetch_20newsgroups_vectorized
 from active_learning.active_learning import ActiveLearner
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import SGDClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import load_breast_cancer
-from sklearn.svm import SVC
-import numpy as np
-import tempfile
-
-
 from scipy.sparse import vstack
 import matplotlib.pyplot as plt
+from copy import deepcopy
 import seaborn as sns
+import numpy as np
 
+
+results_holder = {
+    'least_confident': [],
+    'max_margin': [],
+    'entropy': [],
+}
 
 if __name__ == '__main__':
-    all_random, all_uncertainty = [], []
-    AL = ActiveLearner(strategy='max_margin')
-    clf = LogisticRegression()
+    all_uncertainty_sampling_results = deepcopy(results_holder)
+    all_random_sampling_results = []
     data = fetch_20newsgroups_vectorized()
+    clf = LogisticRegression()
 
     for _ in range(10):
+        uncertainty_sampling_results = deepcopy(results_holder)
+        num_samples, random_sampling_results = [], []
+
         X_train, X_test, y_train, y_test = train_test_split(
             data.data, data.target, test_size=0.5)
         
         X_labeled, X_unlabeled, y_labeled, y_oracle = train_test_split(
-            X_train, y_train, test_size=0.9)
-        
-        num_samples, random, uncertainty = [], [], []
+            X_train, y_train, test_size=0.8)
 
-        for num_queries in (20, 40, 60, 80, 100):
+        for num_queries in (50, 100, 150, 200, 250):
             num_samples.append(num_queries)
 
             random_queries = np.random.choice(X_unlabeled.shape[0], num_queries, replace=False)
             X_augmented = vstack((X_labeled, X_unlabeled[random_queries, :]))
             y_augmented = np.concatenate((y_labeled, y_oracle[random_queries]))
             clf.fit(X_augmented, y_augmented)
-            random.append(np.sum(clf.predict(X_test) == y_test) / np.shape(X_test)[0])
-        
-            clf.fit(X_labeled, y_labeled)
-            idx = AL.rank(clf, X_unlabeled, num_queries)
-            X_augmented = vstack((X_labeled, X_unlabeled[idx, :]))
-            y_augmented = np.concatenate((y_labeled, y_oracle[idx]))
-            clf.fit(X_augmented, y_augmented)
-            uncertainty.append(np.sum(clf.predict(X_test) == y_test) / np.shape(X_test)[0])
+            random_sampling_results\
+                .append(np.sum(clf.predict(X_test) == y_test) / np.shape(X_test)[0])
 
-        all_uncertainty.append(uncertainty)
-        all_random.append(random)
+            for strategy in uncertainty_sampling_results:
+                clf.fit(X_labeled, y_labeled)
+                idx = ActiveLearner(strategy=strategy).rank(clf, X_unlabeled, num_queries)
+                X_augmented = vstack((X_labeled, X_unlabeled[idx, :]))
+                y_augmented = np.concatenate((y_labeled, y_oracle[idx]))
+                clf.fit(X_augmented, y_augmented)
+                uncertainty_sampling_results[strategy]\
+                    .append(np.sum(clf.predict(X_test) == y_test) / np.shape(X_test)[0])
+
+        all_random_sampling_results.append(random_sampling_results)
+        for strategy in uncertainty_sampling_results:
+            all_uncertainty_sampling_results[strategy]\
+                .append(uncertainty_sampling_results[strategy])
 
 
     sns.set_style("darkgrid")
-    plt.plot(num_samples, all_random[0], 'r', num_samples, all_uncertainty[0], 'b')
-    plt.legend(['Random Sampling', 'Least Confident'])
+    plt.plot(num_samples, np.mean(all_random_sampling_results, axis=0), 'red', 
+             num_samples, np.mean(all_uncertainty_sampling_results['least_confident'], axis=0), 'blue',
+             num_samples, np.mean(all_uncertainty_sampling_results['max_margin'], axis=0), 'green',
+             num_samples, np.mean(all_uncertainty_sampling_results['entropy'], axis=0), 'orange',
+    )
+    plt.legend(['Random Sampling', 'Least Confident', 'Max Margin', 'Entropy'], loc=4)
+    plt.ylabel('Error Rate'); plt.xlabel('Number of Queries'); plt.title('20 Newsgroups') 
     plt.show() 
